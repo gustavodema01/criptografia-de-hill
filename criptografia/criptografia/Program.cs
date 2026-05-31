deed8350bef91209cf3f2e512971acc14116c7fe
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-05-31 11:38:46 -0300

    Ajuste no menu e principio da responsabilidade única na Criptografia

--- criptografia/criptografia/Program.cs
+++ criptografia/criptografia/Program.cs
@@ -12,61 +12,52 @@ namespace criptografia
     {
         static async Task Main(string[] args)
         {
-            await Menu(); //sem o await apareceria o write de baixo junto com a função
+            int opcao = await Menu(); //sem o await apareceria o write de baixo junto com a função
             Console.Clear();
 
-            Console.Write("\nDigite a palavra(até 9 letras): ");
-            string palavra = Validacao(); //chama a função Validacao para garantir que escreva letras
-
-            int indice = 0;
-            Console.WriteLine("\nTabela ASCII");
-            foreach (char letra in palavra) //pega um índice de palavra e atribui a letra
+            if (opcao == 1)
             {
-                indice = char.ToLower(letra) - 'a' + 1; // 'a' vale 97 na tabela ASCII. Então: letra - 97 + 1. ex: 'a'(97) - 'a'(97) = 0 + 1 = 1
-                Console.WriteLine($"{char.ToUpper(letra)} = {indice}"); //essa letra é esse número
+                await Criptografar();
             }
-            int tamanho = Tamanhomatrizes(palavra.Length); //dimensão da matriz de acordo com a quantidade de letras em palavra. exp =3x3 = 3, 4x4 = 4...
-
-            string palavraCompleta = CompletarMatriz(palavra, tamanho); //palavra com os *, se necessário
-
-            char[,] Matriz = new char[tamanho, tamanho];
-            Console.Clear();
-
-            indice = 0;
-            Console.WriteLine("Matriz em letras: ");
-            char[,] letras = MatrizLetras(tamanho, Matriz, palavraCompleta, indice);
-
-            int[,] MatrizNum = new int[tamanho, tamanho];
-            indice = 0;
-            Console.WriteLine("\nMatriz em números: ");   //colocar isso em função
-            for (int i = 0; i < tamanho; i++)
+            else if (opcao == 2)
             {
-                for (int k = 0; k < tamanho; k++)
-                {
-                    MatrizNum[i, k] = char.ToLower(palavraCompleta[indice]) - 'a' + 1;
-                    indice++;
-                    if (MatrizNum[i, k] == -54) // o valor do * na tabela ASCII é -54, mas no alfabeto de hill é 0. então atualizei o valor para 0 na matriz
-                    {
-                        MatrizNum[i, k] = 0;
-                    }
-                    Console.Write($"\t{MatrizNum[i, k]}");
-                }
-                Console.WriteLine("");
+                Descriptografia();
             }
-            Console.Clear();
-            int[,] Chave = MatrizChave(tamanho);
-
-            int[,] MatrizCriptografada = Criptografada(MatrizNum, Chave, tamanho, palavra);
-           
         }
 
-        static async Task Menu()//se tem await, a função precisa ser asynk task ao invés de void
+        static async Task<int> Menu()//se tem await, a função precisa ser asynk task ao invés de void
         {
             Console.ForegroundColor = ConsoleColor.Green;
             await Animacao("SEJA BEM VINDO A CRIPTOGRAFIA DE HILL!", 20);
             Console.ResetColor();
+            Console.WriteLine("\n1. Criptografar uma frase");
+            Console.WriteLine("2. Descriptografar uma frase");
+            Console.Write("O que deseja?: ");
+            bool validacao = false;
+            int escolhamenu = 0;
+            while (!validacao)
+            {
+                escolhamenu = ValidacaoInt();
+                validacao = true;
+
+                if (escolhamenu <= 0 | escolhamenu > 2)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    Console.WriteLine("SEJA BEM VINDO A CRIPTOGRAFIA DE HILL!");
+                    Console.ResetColor();
+
+                    Console.WriteLine("\n1. Criptografar uma frase");
+                    Console.WriteLine("2. Descriptografar uma frase");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Escolha uma opção disponível: ");
+                    Console.ResetColor();
+                    validacao = false;
+                }
+            }
             Console.WriteLine("\nAperta uma tecla para continuar");
             Console.ReadKey();
+            return escolhamenu;
         }
         static async Task Animacao(string texto, int velocidade)
         {
@@ -105,7 +96,7 @@ namespace criptografia
                 }
                 else
                 {
-                    foreach(char c in frase)
+                    foreach (char c in frase)
                     {
                         if (!char.IsLetter(c)) //se a frase tiver algo diferente de letras
                         {
@@ -210,18 +201,18 @@ namespace criptografia
         static int[,] Criptografada(int[,] matrizpalavra, int[,] matrizchave, int tamanho, string verso)
         {
             Console.Clear();
-            int[,] resultado = new int[tamanho, tamanho]; 
+            int[,] resultado = new int[tamanho, tamanho];
 
             for (int i = 0; i < tamanho; i++)
             {
                 for (int j = 0; j < tamanho; j++)
                 {
                     resultado[i, j] = 0;
-                    for (int k = 0; k < tamanho; k++)
+                    for (int k = 0; k < tamanho; k++) // linha 0 da coluna 0 da multiplicação de matrizes é o indice 0(k)
                     {
                         resultado[i, j] += matrizchave[i, k] * matrizpalavra[k, j];
                     }
-                    resultado[i, j] = resultado[i, j] % 26; 
+                    resultado[i, j] = resultado[i, j] % 26;
                 }
             }
             Console.Write("Palavra: ");
@@ -240,6 +231,55 @@ namespace criptografia
             }
             return resultado;
         }
+        static async Task<int[,]> Criptografar()
+        {
+            Console.Write("Digite a palavra(até 9 letras): ");
+            string palavra = Validacao(); //chama a função Validacao para garantir que escreva letras
+
+            int indice = 0;
+            Console.WriteLine("\nTabela ASCII");
+            foreach (char letra in palavra) //pega um índice de palavra e atribui a letra
+            {
+                indice = char.ToLower(letra) - 'a' + 1; // 'a' vale 97 na tabela ASCII. Então: letra - 97 + 1. ex: 'a'(97) - 'a'(97) = 0 + 1 = 1
+                Console.WriteLine($"{char.ToUpper(letra)} = {indice}"); //essa letra é esse número
+            }
+            int tamanho = Tamanhomatrizes(palavra.Length); //dimensão da matriz de acordo com a quantidade de letras em palavra. exp =3x3 = 3, 4x4 = 4...
+
+            string palavraCompleta = CompletarMatriz(palavra, tamanho); //palavra com os *, se necessário
+
+            char[,] Matriz = new char[tamanho, tamanho];
+            Console.Clear();
+
+            indice = 0;
+            Console.WriteLine("Matriz em letras: ");
+            char[,] letras = MatrizLetras(tamanho, Matriz, palavraCompleta, indice);
+
+            int[,] MatrizNum = new int[tamanho, tamanho];
+            indice = 0;
+            Console.WriteLine("\nMatriz em números: ");   //colocar isso em função
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int k = 0; k < tamanho; k++)
+                {
+                    MatrizNum[i, k] = char.ToLower(palavraCompleta[indice]) - 'a' + 1;
+                    indice++;
+                    if (MatrizNum[i, k] == -54) // o valor do * na tabela ASCII é -54, mas no alfabeto de hill é 0. então atualizei o valor para 0 na matriz
+                    {
+                        MatrizNum[i, k] = 0;
+                    }
+                    Console.Write($"\t{MatrizNum[i, k]}");
+                }
+                Console.WriteLine("");
+            }
+            Console.Clear();
+            int[,] Chave = MatrizChave(tamanho);
+
+            int[,] MatrizCriptografada = Criptografada(MatrizNum, Chave, tamanho, palavra);
+
+           int Voltarousair = await Voltar();
+            
+            return MatrizCriptografada;
+        }
         static int Determinante(int[,] matriz, int tamanho)
         {
             if (tamanho == 2)
@@ -255,6 +295,41 @@ namespace criptografia
                         matriz[0, 0] * matriz[1, 2] * matriz[2, 1] +
                         matriz[0, 1] * matriz[1, 0] * matriz[2, 2]);
             }
+        }
+        static async Task<int> Voltar()
+        {
+            int opcaomenu = 0;
+            bool validacao = false;
+            Console.WriteLine("\n1. SIM");
+            Console.WriteLine("2. NÃO");
+            Console.Write("Deseja voltar o Menu?: ");
+
+            while (!validacao)
+            {
+                opcaomenu = ValidacaoInt();
+                if(opcaomenu == 1)
+                {
+                   await Menu();
+                }
+                else if(opcaomenu == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Saindo...");
+                    validacao = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Digite uma opção válida: ");
+                    Console.ResetColor();
+                    validacao = false; //volta pra pergunta
+                }
+            }
+            return opcaomenu;
+        }
+        static void Descriptografia()
+        {
+            Console.WriteLine("Função em andamento.");
         }
     }
 }
